1782146e20260a1e00d65528db78eedde98b3fda
Mateusz Tylman <mateusz.tylman@appchance.com>
2015-10-25 16:23:28 +0100

    Initial Commit

---
+++ GameOfThronesWiki/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GameOfThronesWiki
+//
+//  Created by Mateusz Tylman on 25/10/15.
+//  Copyright © 2015 Mateusz Tylman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GameOfThronesWiki/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GameOfThronesWiki
+//
+//  Created by Mateusz Tylman on 25/10/15.
+//  Copyright © 2015 Mateusz Tylman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
